b0baa1111c514ff9910b743e19f28958087aadf8
Scott <dev@misonline.biz>
2015-11-11 09:22:45 -0500

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp=require('gulp'),
+    fs = require('fs-extra'),
+    concat=require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    BUILD_JSON=require('./build.json'),
+    BUILD_NAME='elliptical.soa.js',
+    MIN_NAME='elliptical.soa.min.js',
+    REPO_NAME='elliptical soa',
+    DIST='./dist';
+
+
+gulp.task('default',function(){
+    console.log(REPO_NAME + ' ..."tasks: gulp build|gulp minify"');
+});
+
+gulp.task('build',function(){
+    concatStream(BUILD_NAME)
+        .pipe(gulp.dest(DIST));
+});
+
+gulp.task('minify',function(){
+    concatStream(MIN_NAME)
+        .pipe(uglify())
+        .pipe(gulp.dest(DIST));
+});
+
+function srcStream(src){
+    if(src===undefined) src=BUILD_JSON;
+    return gulp.src(src);
+}
+
+function concatStream(name,src){
+    if(src===undefined) src=BUILD_JSON;
+    return srcStream(src)
+        .pipe(concat(name))
+}
+
+function fileStream(src,dest){
+    gulp.src(src)
+        .pipe(gulp.dest(dest));
+}
+
+function concatFileStream(src,dest,name){
+    gulp.src(src)
+        .pipe(concat(name))
+        .pipe(gulp.dest(dest));
+}
+
+function minFileStream(src,dest,name){
+    gulp.src(src)
+        .pipe(concat(name))
+        .pipe(uglify())
+        .pipe(gulp.dest(dest));
+}
---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+module.exports={
+    Service:require('./lib/service'),
+    Provider:require('./lib/provider'),
+    factory:require('./lib/factory'),
+    noop:require('./lib/noop'),
+    interval:require('./lib/interval'),
+    debounce:require('./lib/debounce'),
+    $Cookie:require('./lib/provider.cookie'),
+    $Local:require('./lib/provider.local'),
+    $Session:require('./lib/provder.session'),
+    $OData:require('./lib/provider.odata'),
+    $Rest:require('/lib/provider.rest'),
+    $Template:require('./lib/provider.template'),
+    $Validation:require('./lib/provider.validation'),
+    $Pagination:require('./lib/provider.pagination'),
+    $Sort:require('./lib/provider.sort'),
+    $Navigation:require('./lib/provder.navigation'),
+    Notify:require('./lib/services'),
+    Validation:require('./lib/services'),
+    Search:require('./lib/services'),
+    Dialog:require('./lib/services'),
+    Store:require('./lib/services'),
+    Sort:require('./lib/services')
+};
